4da4262219f75cf0b642e6209caefd9c4c1a53b4
DougWelchons <doug.welchons@gmail.com>
2021-04-18 14:02:44 -0600

    Add basic sad path testing foir mail api endpoint

--- spec/requests/post_mail_request_spec.rb
+++ spec/requests/post_mail_request_spec.rb
@@ -11,13 +11,82 @@ include Rack::Test::Methods
     ApplicationController
   end
 
-  it 'can send emails' do
-    VCR.use_cassette('send_email') do
-      post '/api/v1/mail?to=doug.welchons@gmail.com&from=angelbreaux@hotmail.com&subject=BDAY BASH&content=You are Invited to my party!'
-      body = JSON.parse(last_response.body, symbolize_names: true)
+  describe "Happy path" do
+    it 'can send emails' do
+      VCR.use_cassette('send_email') do
+        post '/api/v1/mail?to=doug.welchons@gmail.com&from=angelbreaux@hotmail.com&subject=BDAY BASH&content=You are Invited to my party!'
+        body = JSON.parse(last_response.body, symbolize_names: true)
 
-      expect(last_response.status).to eq(202)
-      expect(body[:message]).to eq("Message sent sucscessfully")
+        expect(last_response.status).to eq(202)
+        expect(body[:message]).to eq("Message sent sucscessfully")
+      end
+    end
+  end
+
+  describe "Sad path" do
+    describe "returns a 400 error if params are missing" do
+      it "must have a sender" do
+        post '/api/v1/mail?to=doug.welchons@gmail.com&subject=BDAY BASH&content=You are Invited to my party!'
+        body = JSON.parse(last_response.body, symbolize_names: true)
+
+        expect(last_response.status).to eq(400)
+        expect(body[:message]).to eq("There was an error proccessing your request")
+      end
+
+      it "sender cannot be blank" do
+        post '/api/v1/mail?to=doug.welchons@gmail.com&from=&subject=BDAY BASH&content=You are Invited to my party!'
+        body = JSON.parse(last_response.body, symbolize_names: true)
+
+        expect(last_response.status).to eq(400)
+        expect(body[:message]).to eq("There was an error proccessing your request")
+      end
+
+      it "must have a recipient" do
+        post '/api/v1/mail?from=doug.welchons@gmail.com&subject=BDAY BASH&content=You are Invited to my party!'
+        body = JSON.parse(last_response.body, symbolize_names: true)
+
+        expect(last_response.status).to eq(400)
+        expect(body[:message]).to eq("There was an error proccessing your request")
+      end
+
+      it "recipient cannot be blank" do
+        post '/api/v1/mail?to=&from=doug.welchons@gmail.com&subject=BDAY BASH&content=You are Invited to my party!'
+        body = JSON.parse(last_response.body, symbolize_names: true)
+
+        expect(last_response.status).to eq(400)
+        expect(body[:message]).to eq("There was an error proccessing your request")
+      end
+
+      it "must have a subject" do
+        post '/api/v1/mail?to=doug.welchons@gmail.com&from=angelbreaux@hotmail.com&content=You are Invited to my party!'
+        body = JSON.parse(last_response.body, symbolize_names: true)
+
+        expect(last_response.status).to eq(400)
+        expect(body[:message]).to eq("There was an error proccessing your request")
+      end
+
+      it "subject cannot be blank" do
+        post '/api/v1/mail?to=doug.welchons@gmail.com&from=angelbreaux@hotmail.com&subject=&content=You are Invited to my party!'
+        body = JSON.parse(last_response.body, symbolize_names: true)
+
+        expect(last_response.status).to eq(400)
+        expect(body[:message]).to eq("There was an error proccessing your request")
+      end
+      it "must have content" do
+        post '/api/v1/mail?to=doug.welchons@gmail.com&from=angelbreaux@hotmail.com&subject=BDAY BASH'
+        body = JSON.parse(last_response.body, symbolize_names: true)
+
+        expect(last_response.status).to eq(400)
+        expect(body[:message]).to eq("There was an error proccessing your request")
+      end
+
+      it "content cannot be blank" do
+        post '/api/v1/mail?to=doug.welchons@gmail.com&from=angelbreaux@hotmail.com&subject=BDAY BASH&content='
+        body = JSON.parse(last_response.body, symbolize_names: true)
+
+        expect(last_response.status).to eq(400)
+        expect(body[:message]).to eq("There was an error proccessing your request")
+      end
     end
   end
 end
